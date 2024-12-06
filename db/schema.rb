2082564bd35ceebf803c9e1ba741cd310392b69a
Leah Perri <perrileah2023@gmail.com>
2024-12-06 12:48:23 -0600

    changes pub date to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_06_183409) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_06_184731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_06_183409) do
     t.string "title"
     t.string "author"
     t.string "isbn"
-    t.date "publication_date"
+    t.string "publication_date"
     t.string "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
