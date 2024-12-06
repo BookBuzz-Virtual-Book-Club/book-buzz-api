c2e5f637e276597bb214b3c91310258082e7960a
Leah Perri <perrileah2023@gmail.com>
2024-12-06 12:52:21 -0600

    removes isbn column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_06_184731) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_06_185119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,7 +25,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_06_184731) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
-    t.string "isbn"
     t.string "publication_date"
     t.string "genre"
     t.datetime "created_at", null: false
